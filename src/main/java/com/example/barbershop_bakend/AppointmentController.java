bbeab740649cc3d4811fc26d983f75c8db42ed61
ll-laila <lailatimasli1@gmail.com>
2024-12-15 05:49:02 +0100

    deploy backend

--- src/main/java/com/example/barbershop_bakend/AppointmentController.java
+++ src/main/java/com/example/barbershop_bakend/AppointmentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://barbershop-frontend-black.vercel.app")
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
